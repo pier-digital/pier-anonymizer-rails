ff1c17470df3f9bc4b1d8115a358e119a0cbd078
João Holanda <joaoholandajr@gmail.com>
2020-11-20 07:28:28 -0300

    Add comments

--- lib/pier_anonymizer.rb
+++ lib/pier_anonymizer.rb
@@ -3,6 +3,22 @@ require 'digest'
 
 module PierAnonymizer
   def get_hash(value)
+  
+    # To difficult the discovering of the original value,
+    # as also getting a pretty smaller string instead of the generated sha256 value
+    # (e.g. because of database column size constraints),
+    # we generate the anonymized value from two different substrings of the generated hash.
+
+    # ===== Why do not apply Salt or Pepper?
+    #  Just to make it simple.
+    #  There is no need to create another rule or structure for salts and pepper.
+    #  And, in the end, we're gonna need to get just a substring of the hash.
+    # =====
+
+    # ===== Then notice:
+    #  The intervals 10..17 and 30..37 are just magic numbers.
+    #  However, it must be obeyed anywhere in order to always generate the same value.
+
     return nil if value.nil?
 
     hashed = Digest::SHA256.hexdigest value.downcase
